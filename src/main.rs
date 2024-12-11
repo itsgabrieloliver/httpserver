97e973859a8126971a84f6e9f635d2fe37e41d2c
Gabriel Oliver <gabrielo@withnubo.com>
2024-12-11 15:02:05 -0500

    feat: add sleepy route and add match

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
+use core::time;
 use std::fs;
 use std::io::prelude::*;
 use std::io::BufReader;
 use std::net::TcpListener;
 use std::net::TcpStream;
+use std::thread;
 
 use httpserver::ThreadPool;
 
@@ -39,13 +41,16 @@ fn handle_connection(mut stream: TcpStream) {
 
     println!("{request_line}");
 
-    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
-        ("HTTP/1.1 200 OK", "html/index.html")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND", "html/404.html")
+    let (status_line, file_name) = match &request_line[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "html/index.html"),
+        "GET /sleepy HTTP/1.1" => {
+            thread::sleep(time::Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "html/index.html")
+        }
+        _ => ("HTTP/1.1 404 NOT FOUND", "html/404.html"),
     };
 
-    let contents = fs::read_to_string(filename).unwrap();
+    let contents = fs::read_to_string(file_name).unwrap();
     let length = contents.len();
 
     let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
